1314f052e3c177e080d09b6dad3db3cc8aa8c319
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-09-24 12:01:35 +0300

    remove empty line

--- lab2/parse/parse_file.py
+++ lab2/parse/parse_file.py
@@ -4,7 +4,9 @@ from parse.file_op import read_file
 
 
 def parse_file(file_name):
-    transitions = read_file(file_name).split(sep='\n')
+    transitions = set(read_file(file_name).split(sep='\n'))
+    transitions.remove("")
+    transitions = sorted(list(transitions))
 
     valid = [re.fullmatch(r'q\d,.=[qf]\d', s) for s in transitions]
     if None in valid:
