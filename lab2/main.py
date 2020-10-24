0a3e024c25e9378d3852a1205d3dd27b74e55a7d
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-10-24 14:58:43 +0300

    done to_deterministic.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,10 +1,13 @@
 from check_deterministic import check_deterministic
 from draw import draw
 from parse.parse_file import parse_file
+from to_deterministic import to_deterministic
 
 if __name__ == "__main__":
     # file_name = str(input())
     file_name = "var2.txt"
     graph = parse_file(file_name)
     # draw(graph)
+    # print(graph.adj)
     check_deterministic(graph)
+    to_deterministic(graph)
---
+++ lab2/to_deterministic.py
@@ -0,0 +1,43 @@
+import networkx as nx
+
+
+def to_deterministic(graph: nx.DiGraph):
+    """ Перевод из недетерминированного автомата в детерминированный
+
+        @:return nx.DiGraph - недетерминированный автомат
+    """
+    new_graph = nx.DiGraph()
+    join_vertex = []
+
+    for key_begin in graph.adj.keys():
+        begin = key_begin
+
+        for v in join_vertex:
+            if v.find(key_begin) != -1:
+                begin = v
+
+        weigths = {}
+        for key_end in graph.adj[key_begin].keys():
+            w = graph.adj[key_begin][key_end]['weight']
+            is_not_loop = begin.find(key_end) == -1
+
+            if w not in weigths.keys() and is_not_loop:
+                weigths[w] = []
+            if is_not_loop:  # не петля
+                weigths[w].append(key_end)
+
+        # print(weigths)
+        for key_w in weigths:
+            if len(weigths[key_w]) > 1:
+                end = ''.join(map(str, weigths[key_w]))
+                join_vertex.append(end)
+                print(end)
+                new_graph.add_edge(begin, end, weigth=key_w)
+            else:
+                end = weigths[key_w][0]
+                for v in join_vertex:
+                    if v.find(end) != -1:
+                        end = v
+                new_graph.add_edge(begin, end, weigth=key_w)
+
+    print(new_graph.adj)
