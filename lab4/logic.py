8523358b0311b37393b75d2f864fa218b69195f8
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-12-17 21:16:28 +0300

    FIRST FIX with recursive descent

--- lab4/logic.py
+++ lab4/logic.py
@@ -6,6 +6,7 @@ from parse.parse_file import EMPTY_STR_SYMB
 def find_set_first(rule, rules_dict) -> list:
     ans = []
     tokens = set()
+    res = []
 
     for right in rule.right:
         first_char = right[0]
@@ -13,16 +14,18 @@ def find_set_first(rule, rules_dict) -> list:
             end = right.find(">")
             non = right[:end+1]
 
-            ans.append((rule.left, tokens))
             rules_dict[non].need_to_check = False
-            return ans + find_set_first(rules_dict[non], rules_dict)
+            res += find_set_first(rules_dict[non], rules_dict)
+
         elif first_char == '‘':  # terminal
             end = right.find("’")
             s = {right[1:end]}
             tokens |= s
-        elif first_char == EMPTY_STR_SYMB:
-            continue
+        elif first_char == EMPTY_STR_SYMB:  # Надо исправить
+            s = {EMPTY_STR_SYMB}
+            tokens |= s
 
+    ans += res
     ans.append((rule.left, tokens))
     return ans
 
@@ -39,6 +42,7 @@ def get_first(rules, rules_dict: dict):
     """
     first_dict = {}
     # for rule in rules:
+    print()
     for key in rules_dict.keys():
         if rules_dict[key].need_to_check:
             ans = find_set_first(rules_dict[key], rules_dict)
