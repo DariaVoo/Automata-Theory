f92188524e045f72484f6bf03b1c008bf9fc3827
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-12-17 20:36:23 +0300

    FIRST done with recursive descent

--- lab4/logic.py
+++ lab4/logic.py
@@ -14,6 +14,7 @@ def find_set_first(rule, rules_dict) -> list:
             non = right[:end+1]
 
             ans.append((rule.left, tokens))
+            rules_dict[non].need_to_check = False
             return ans + find_set_first(rules_dict[non], rules_dict)
         elif first_char == '‘':  # terminal
             end = right.find("’")
@@ -25,7 +26,8 @@ def find_set_first(rule, rules_dict) -> list:
     ans.append((rule.left, tokens))
     return ans
 
-def get_first(rules, rules_dict):
+
+def get_first(rules, rules_dict: dict):
     """
     FIRST(E) = set{} -
     возвращает словарь всех FIRST вида
@@ -36,23 +38,11 @@ def get_first(rules, rules_dict):
     :return:
     """
     first_dict = {}
-    for rule in rules:
-        # tokens = set()
-        # for right in rule.right:
-        #     first_char = right[0]
-        #     if first_char == '<':
-        #         #   recursion
-        #         continue
-        #     elif first_char == '‘':
-        #         end = right.find("’")
-        #         s = {right[1:end]}
-        #         tokens |= s
-        #     elif first_char == EMPTY_STR_SYMB:
-        #         continue
-
-        ans = find_set_first(rule, rules_dict)
-        print('ANS: ', ans)
-        # first_dict[rule.left] = tokens
+    # for rule in rules:
+    for key in rules_dict.keys():
+        if rules_dict[key].need_to_check:
+            ans = find_set_first(rules_dict[key], rules_dict)
+            print('ANS: ', ans)
 
     print('\nFIRST:', first_dict)
     return first_dict
