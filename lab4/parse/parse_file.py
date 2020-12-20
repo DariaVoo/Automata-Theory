30dc3d1e80c562f5fbcb1cb62fd05b3455e7c838
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-12-20 23:06:54 +0300

    fix get input and test file

--- lab4/parse/parse_file.py
+++ lab4/parse/parse_file.py
@@ -120,8 +120,8 @@ def parse_file(file_name):
 
 
 def get_input(file_name):
-    input_str:str = read_file(file_name)
-    new_str = input_str.replace('\n', EMPTY_STR_SYMB)
-    new_str += EMPTY_STR_SYMB
+    input_str: str = read_file(file_name)
+    new_str = input_str.replace('\n', '')
+    new_str += ' ' + EMPTY_STR_SYMB
     print(f'Input: {new_str}')
     return new_str
