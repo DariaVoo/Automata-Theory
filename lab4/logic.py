a54b59c2d6637f2cbb4cdf18d272323db27ca874
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-12-20 23:49:47 +0300

    almost done but need to fix loop production

--- lab4/logic.py
+++ lab4/logic.py
@@ -3,7 +3,7 @@ import pandas as pd
 from parse.parse_file import EMPTY_STR_SYMB
 
 
-def predictive_analysis(table, input_str, terminals):
+def predictive_analysis(table: pd.DataFrame, input_str, terminals):
     stack = [EMPTY_STR_SYMB, '<program>']
     input_str = input_str.split()
     index = 0
@@ -17,16 +17,36 @@ def predictive_analysis(table, input_str, terminals):
             a = stack.pop()
             print(f'pop from stack {a}')
             index += 1
+        elif stack[-1].find('><') != -1:
+            print(f'get concat production {stack[-1]}')
+            # закидываем на стек левое значение конкатенации
+            end = stack[-1].find(">")
+            a = stack[-1][:end+1]
+            stack.pop()
+            stack.append(a)
+
+            # рассматриваем это слово из строки посимвольно
+            li = list(input_str[index])
+            input_str = input_str[:index] + li + input_str[index + 1:]
         elif stack[-1] in terminals:
             print(f'Error: Wrong terminal on stack {stack[-1]}')
             print(f'Remove terminal {stack[-1]} from stack!')
             stack.pop()
         else:
-            production = table.loc[stack[-1], input_str[index]]
+            col = input_str[index]
+            if col not in table.columns:
+                col = EMPTY_STR_SYMB
+            production = table.loc[stack[-1], col]
             if type(production) != float:
                 a = stack.pop()
                 stack += production
                 print(f'Use production {a} -> {production}')
+
             else:
-                print('Error, table is empty!')
-                break
+                if stack[-1] == '<assign_end>':
+                    # рассматриваем это слово из строки посимвольно
+                    li = list(input_str[index])
+                    input_str = input_str[:index] + li + input_str[index + 1:]
+                else:
+                    print('Error, table is empty!')
+                    break
