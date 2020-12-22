cec1034caa767717fbc1cf838bef11601082f7d7
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-12-22 18:30:19 +0300

    add grammar2

--- lab4/main.py
+++ lab4/main.py
@@ -4,8 +4,9 @@ from logic import predictive_analysis
 from make_table import make_table
 from parse.parse_file import parse_file, get_terminals_and_non, make_transitions, get_input
 
-if __name__ == '__main__':
 
+
+def syntax_analysis_lab4():
     rules, main_rules = parse_file('grammar')
     terminals, nonterminals = get_terminals_and_non(main_rules)
     dict_rule, dict_rule_full = make_transitions(main_rules)
@@ -15,5 +16,18 @@ if __name__ == '__main__':
     print()
     input_str = get_input('test.txt')
     predictive_analysis(table, input_str, terminals, 'log.txt')
+
+
+if __name__ == '__main__':
+
+    rules, main_rules = parse_file('grammar2')
+    terminals, nonterminals = get_terminals_and_non(main_rules)
+    dict_rule, dict_rule_full = make_transitions(main_rules)
+    first = get_first(dict_rule)
+    follow = get_follow(first, dict_rule_full)
+    table = make_table(follow, main_rules, terminals)
+    print()
+    # input_str = get_input('test.txt')
+    # predictive_analysis(table, input_str, terminals, 'log.txt')
 
 
